9d4f018c962434f6d8bdd5312b6538a7a92ef171
Alexander Nizov <nizoff.as@yandex.ru>
2023-04-06 22:14:14 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,17 +1,18 @@
 import pandas as pd
 import numpy as np
 
-from scipy.stats import norm
+from scipy.stats import gamma
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 457863109 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
+    koef = 2/(56*56)
     alpha = 1 - p
-    loc = x.mean()
-    scale = np.sqrt(np.var(x)) / np.sqrt(len(x))
-    return loc - scale * norm.ppf(1 - alpha / 2), \
-           loc - scale * norm.ppf(alpha / 2)
+    quan_1 = gamma(len(x), 1).ppf((1-alpha)/2)
+    quan_2 = gamma(len(x), 1).ppf((1+alpha)/2)
+    sum = np.sum(x)
+    return (koef*((quan_1+sum)/2)-1/2, koef*((quan_2+sum)/2)-1/2)
